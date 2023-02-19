15f64bc3f9362f44a430701b080d6b6ffd39de93
EllieNguyen <ellieN1235@gmail.com>
2023-02-18 16:36:02 -0800

    Initial Commit

---
+++ recipe list app/recipe_list_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_list_appApp.swift
+//  recipe list app
+//
+//  Created by Ellie on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct recipe_list_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
